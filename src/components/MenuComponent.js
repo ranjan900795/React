4877cbf07e4bf086878c858683cd24a6d92d0811
Shubham Ranjan <shubhamranjan542@gmail.com>
2020-06-15 12:04:54 +0530

    Controlled Form

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -32,6 +32,20 @@ import { Link } from 'react-router-dom';
         return (            
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     {menu}
                 </div>                
             </div>
